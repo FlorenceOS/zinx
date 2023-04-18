381694f3e533e870534a041c7331be4762ab7cfb
Hannes Bredberg <hannesbredberg@gmail.com>
2023-04-18 20:06:04 +0200

    Fix binary name

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "pkgz",
+        .name = "zinx",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
